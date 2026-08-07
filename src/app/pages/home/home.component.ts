6ee388d08a14bb3a140a7b612de41da4b58c3336
fabian-mendoza_wtw <Fabian.Mendoza@willistowerswatson.com>
2026-08-07 12:38:16 -0500

    Update element task

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -8,21 +9,66 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  tasks = signal([
-    'Instalar Angular CLI',
-    'Crear un proyecto Angular',
-    'Crear un componente',
-    'Crear un servicio',
-    'Crear un modelo',
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Instalar Angular CLI',
+      completed: false
+    },
+    {
+      id: Date.now() + 1,
+      title: 'Crear un proyecto Angular',
+      completed: false
+    },
+    {
+      id: Date.now() + 2,
+      title: 'Crear un componente',
+      completed: false
+    },
+    {
+      id: Date.now() + 3,
+      title: 'Crear un servicio',
+      completed: false
+    },
+    {
+      id: Date.now() + 4,
+      title: 'Crear un modelo',
+      completed: false
+    }
   ]);
 
   changeHandler(event: Event) {
     const input = event.target as HTMLInputElement;
-    const newTask = input.value;
+    const title = input.value;
+    this.addTask(title);
+    input.value = '';
+  }
+
+  addTask(title: string) {
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false
+    }
+
     this.tasks.update((currentTasks) => [...currentTasks, newTask]);
   }
 
   deleteTask(index: number) {
     this.tasks.update((currentTasks) => currentTasks.filter((_, position) => position !== index));
+  }
+
+  updateTask(index: number) {
+    this.tasks.update((currentTasks) => {
+      return currentTasks.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            completed: !task.completed
+          }
+        }
+        return task;
+      });
+    });
   }
 }
